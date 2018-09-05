a276a56f364635e68ccb527b5c1d9ea9d749c2d6
Daniel LACROIX <dlacroix@erasme.org>
2018-09-05 11:23:23 +0200

    provide server side usages stats

--- LaclasseService/Directory/Logs.cs
+++ LaclasseService/Directory/Logs.cs
@@ -28,6 +28,8 @@
 using System;
 using System.Net;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 using Erasme.Http;
 using Erasme.Json;
 using Laclasse.Authentication;
@@ -96,6 +98,230 @@ namespace Laclasse.Directory
 		{
 			// API only available to authenticated users
 			BeforeAsync = async (p, c) => await c.EnsureIsAuthenticatedAsync();
+
+			GetAsync["/stats"] = async (p, c) =>
+			{
+				var mode = "4WEEK";
+				if (c.Request.QueryString.ContainsKey("mode"))
+					mode = c.Request.QueryString["mode"];
+				var start = DateTime.Parse(c.Request.QueryString["timestamp>"]);
+				var end = DateTime.Parse(c.Request.QueryString["timestamp<"]);
+                var authUser = await c.GetAuthenticatedUserAsync();    
+                var filterAuth = (new Log()).FilterAuthUser(authUser);
+				SearchResult<Log> result;
+                using (DB db = await DB.CreateAsync(dbUrl))
+                {
+                    result = await Model.SearchAsync<Log>(db, c, filterAuth);
+                    foreach (var item in result.Data)
+                        await item.EnsureRightAsync(c, Right.Read, null);
+                }
+                c.Response.StatusCode = 200;
+
+                // calcul stats
+				var totalCount = 0;
+				// sso types
+				var ssoProfiles = new Dictionary<string,long>();
+				var ssoTypes = new Dictionary<string,long>();
+				var netTypes = new Dictionary<string, long>();
+				var resources = new Dictionary<int, Dictionary<string,int>>();
+				var structures = new Dictionary<string, Dictionary<string,int>>();
+				var users = new Dictionary<string, Dictionary<string,int>>();
+
+				var maxY = 0;
+				var lastDataHour = new DateTime(start.Year, start.Month, start.Day, start.Hour, 0, 0);
+				var startHour = lastDataHour;
+				var currentHour = DateTime.MinValue;
+                var count = 0; var first = true;
+				var hours = new List<HourValue>();
+
+				if (mode == "4WEEK")
+				{
+					for (var i = 0; i < 4; i++) {
+						var weekNb = WeekOfYear(new DateTime(start.Ticks + (3600L * 24L * 7L * 10000000L * i) + (3600L * 24L * 3L * 10000000L)));
+						var k = weekNb.Year.ToString("0000") + "-" + weekNb.Week.ToString("00");
+						users[k] = new Dictionary<string, int>();
+					}
+				}
+                else
+				{
+					for (var i = 0; i < ((mode == "6MONTH") ? 6 : 12); i++) {
+						var month = start.AddMonths(i);
+						var m = month.Year.ToString("0000") + "-" + month.Month.ToString("00");
+						users[m] = new Dictionary<string, int>();
+					}
+				}
+                
+
+				foreach (var log in result.Data)
+				{
+					totalCount++;
+					if (log.application_id == "SSO" && log.parameters != null)
+					{
+						var pt = log.parameters.Split('&');
+						var ph = new Dictionary<string, string>();
+						foreach (var pi in pt)
+						{
+							var at = pi.Split('=');                  
+							ph[Uri.UnescapeDataString(at[0])] = at[1];
+						}
+						if (ph.ContainsKey("idp"))
+						{
+							if (!ssoTypes.ContainsKey(ph["idp"]))
+								ssoTypes[ph["idp"]] = 1;
+							else
+								ssoTypes[ph["idp"]] += 1;
+						}
+						if (log.profil_id != null)
+						{
+							if (!ssoProfiles.ContainsKey(log.profil_id))
+								ssoProfiles[log.profil_id] = 1;
+							else
+								ssoProfiles[log.profil_id] += 1;
+						}
+						if (log.ip != null)
+						{
+							var intern = log.ip.StartsWith("193.51.226.", StringComparison.InvariantCulture);
+							var key = intern ? "Interne collège" : "Internet";
+							if (!netTypes.ContainsKey(key))
+								netTypes[key] = 1;
+							else
+								netTypes[key] += 1;
+						}
+					}
+					if (log.resource_id != null) 
+					{
+						if (!resources.ContainsKey((int)log.resource_id))
+							resources[(int)log.resource_id] = new Dictionary<string,int>();
+						if (!resources[(int)log.resource_id].ContainsKey(log.user_id))
+							resources[(int)log.resource_id][log.user_id] = 1;
+						else
+							resources[(int)log.resource_id][log.user_id] += 1;
+					}
+					if (log.structure_id != null) 
+                    {
+						if (!structures.ContainsKey(log.structure_id))
+							structures[log.structure_id] = new Dictionary<string,int>();
+						if (!structures[log.structure_id].ContainsKey(log.user_id))
+							structures[log.structure_id][log.user_id] = 1;
+                        else
+							structures[log.structure_id][log.user_id] += 1;
+                    }
+					if (mode == "4WEEK")
+					{
+						var time = log.timestamp;
+                        var weekNb = WeekOfYear(time);
+						var k = weekNb.Year.ToString("0000") + "-" + weekNb.Week.ToString("00");
+						if (users.ContainsKey(k)) {
+							if (!users[k].ContainsKey(log.user_id))
+								users[k][log.user_id] = 1;
+                            else
+								users[k][log.user_id] += 1;
+                        }
+					}
+					else
+					{
+						var time = log.timestamp;
+						var m = time.Year.ToString("0000") + "-" + time.Month.ToString("00");
+						if (users.ContainsKey(m)) {
+							if (!users[m].ContainsKey(log.user_id))
+								users[m][log.user_id] = 1;
+							else
+								users[m][log.user_id] += 1;
+						}
+					}
+                    // handle nb logs per hours
+					var timestamp = log.timestamp;
+                    var hour = new DateTime(timestamp.Year, timestamp.Month, timestamp.Day, timestamp.Hour, 0, 0);
+                    if (first) {
+                        first = false;
+                        currentHour = hour;
+                        count++;
+                    }
+                    else if (hour == currentHour)
+                        count++;
+                    else {
+                        while (lastDataHour < currentHour) {
+							hours.Add(new HourValue { Time = lastDataHour, Value = 0 });                     
+							lastDataHour = lastDataHour.AddHours(1);
+                        }
+                        if (count > maxY)
+                            maxY = count;
+                        hours.Add(new HourValue { Time = currentHour, Value = count });
+						lastDataHour = lastDataHour.AddHours(1);
+                        currentHour = hour;
+                        count = 1;
+                    }
+				}
+
+				var ssoNames = new Dictionary<string,string> {
+                    ["ENT"] = "Compte laclasse",
+					["AAF"] = "Compte Académique",
+					["CUT"] = "Compte Grand Lyon Connect",
+					["EMAIL"] =  "Récup. par email",
+					["SMS"] = "Récup. par SMS"
+                };
+                
+				c.Response.Content = new JsonObject
+				{
+					["count"] = totalCount,
+					["connections"] = new JsonArray(ssoTypes.Select((arg) => new JsonObject {
+						["name"] = ssoNames.ContainsKey(arg.Key) ? ssoNames[arg.Key] : arg.Key,
+						["value"] = arg.Value
+					})),
+					["networks"] = new JsonArray(netTypes.Select((arg) => new JsonObject {
+						["name"] = arg.Key,
+                        ["value"] = arg.Value
+					})),
+					["profiles"] = new JsonArray(ssoProfiles.Select((arg) => new JsonObject {
+                        ["name"] = arg.Key,
+                        ["value"] = arg.Value
+                    })),
+					["resources"] = new JsonArray(resources.Select((arg) => new JsonObject {
+						["name"] = arg.Key,
+						["value"] = arg.Value.Keys.Count()
+					}).OrderByDescending((arg) => (int)arg["value"])),
+					["structures"] = new JsonArray(structures.Select((arg) => new JsonObject {
+                        ["name"] = arg.Key,
+                        ["value"] = arg.Value.Keys.Count()
+					}).OrderByDescending((arg) => (int)arg["value"])),
+					["users"] = new JsonArray(users.Select((arg) => new JsonObject {
+                        ["name"] = arg.Key,
+                        ["value"] = arg.Value.Keys.Count()
+                    })),
+					["activity"] = new JsonObject {
+						["maxY"] = maxY,
+						["start"] = startHour,
+						["data"] = new JsonArray(hours.Select((arg) => new JsonPrimitive(arg.Value)))
+					}
+				};
+			};
 		}
+
+        public struct HourValue
+		{
+			public DateTime Time;
+			public int Value;
+		}
+
+        public struct WeekOfYearResult
+		{
+			public int Week;
+			public int Year;
+		}
+
+		public static WeekOfYearResult WeekOfYear(DateTime day)
+		{
+			var year = day.Year;
+            var jan4 = new DateTime(year, 1, 4);
+			var week1day = new DateTime(jan4.Ticks - ((((int)jan4.DayOfWeek - 1) % 7) * 24 * 60 * 60 * 10000000));
+            var weekNb = ((day.Ticks - week1day.Ticks) / (24L * 60L * 60L * 10000000L * 7L)) + 1;
+            if (weekNb < 1) {
+                year--;
+                jan4 = new DateTime(year, 1, 4);
+				week1day = new DateTime(jan4.Ticks - ((((int)jan4.DayOfWeek - 1) % 7) * 24 * 60 * 60 * 10000000));
+				weekNb = ((day.Ticks - week1day.Ticks) / (24L * 60L * 60L * 10000000L * 7L)) + 1;
+            }
+			return new WeekOfYearResult { Week = (int)weekNb, Year = year };
+        }
 	}
 }
